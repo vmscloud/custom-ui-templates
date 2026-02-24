ee7d802b0af805da069a10e21125f58e1eb2f0e3
DongJuKim-Carlo <147456232+jook1356a@users.noreply.github.com>
2026-02-24 16:24:23 +0900

    Feat: 기본 레이아웃 추가

--- frontend/src/components/DeveloperTool/DeveloperTool.vue
+++ frontend/src/components/DeveloperTool/DeveloperTool.vue
@@ -9,6 +9,7 @@
 <script setup lang="ts">
 import DeveloperToolCore from "@/components/DeveloperTool/DeveloperToolCore.vue";
 import {computed} from "vue";
+import './styles/index.scss'
 
 const isDev = computed(() => {
   // Module Federation으로 로드된 경우 __remotes__가 정의되어 있지 않음
